e12cb3a82d1a22add7bf0b6e7e25df3e13d4eed7
KING6CODE <alistpassecoutrin@gmail.com>
2025-08-29 00:29:44 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 export const metadata = {
